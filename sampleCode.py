405b4a8cf44f736dfc23905c736e390bc1fcaf9f
Dominique Evans <105753151+A174411@users.noreply.github.com>
2022-05-23 11:46:59 -0400

    Add files via upload

---
+++ sampleCode.py
@@ -0,0 +1,9 @@
+"""
+Create user input for if they are hungry
+"""
+
+user_input = input("Hello, are you hungry? ")
+if user_input == "yes":
+    print("Eat a banana")
+else:
+    print("Then, read a book")
