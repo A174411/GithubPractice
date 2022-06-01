e840f39e82a6e8e1fd6bff3f9546ae03816a183a
Dominique Evans <Dominique_Evans@Progressive.com>
2022-06-01 15:08:05 -0400

    test

---
+++ sampleCode.py
@@ -0,0 +1,13 @@
+"""
+Create user input for if they are hungry
+"""
+
+user_input = input("Hello, are you hungry? ")
+if user_input == "yes":
+    print("Do you like bananas? ")
+    if input == "yes":
+        print("Eat a banana.")
+    else:
+        print("That's too bad. Banana's are a good source of potassium.")
+else:
+    print("Then, read a book")
